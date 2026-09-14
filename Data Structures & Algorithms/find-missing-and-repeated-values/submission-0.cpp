a47c7009a0b9c0e54f9ee13f1b4c4d259adaf9e7
Kusuma Harish <138509145+KUSUMAHARISH@users.noreply.github.com>
2026-09-14 08:18:13 +0530

    Add: find-missing-and-repeated-values - submission-0

---
+++ Data Structures & Algorithms/find-missing-and-repeated-values/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        map<int,int>mp;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                mp[grid[i][j]]++;
+            }
+        }
+        int first = -1,second = -1;
+        for(int i=1;i<=n*n;i++){
+            if(mp[i] == 2){
+                first = i;
+            }
+            if(mp[i] == 0){
+                second = i;
+            }
+        }
+        return {first, second};
+    }
+};
