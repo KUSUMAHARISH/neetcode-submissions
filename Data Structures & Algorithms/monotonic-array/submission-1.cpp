edfc010afab35b62e89eb48aab898cac74f17195
Kusuma Harish <138509145+KUSUMAHARISH@users.noreply.github.com>
2026-09-14 15:02:48 +0530

    Add: monotonic-array - submission-1

---
+++ Data Structures & Algorithms/monotonic-array/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& arr) {
+         int n = arr.size();
+
+    bool increasing = true;
+    bool decreasing = true;
+
+    for (int i = 1; i < n; i++) {
+
+        if (arr[i] < arr[i - 1])
+            increasing = false;
+
+        if (arr[i] > arr[i - 1])
+            decreasing = false;
+    }
+
+    return increasing || decreasing;
+    }
+};
